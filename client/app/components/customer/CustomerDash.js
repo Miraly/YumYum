fa2acb82dbb2a29d1940597bcdae5095ae48699d
Justin Bellero <jpbellero@gmail.com>
2017-04-29 12:14:06 -0400

    fixed route and merged test-router

--- client/app/components/customer/CustomerDash.js
+++ client/app/components/customer/CustomerDash.js
@@ -4,9 +4,11 @@ const React = require("react");
 const CustomerDash = React.createClass({
 
 	render: function(){
-		return(
-      <CustomerFilterHeader />
-      {this.props.children}
+		return (
+			<div>
+      	<CustomerFilterHeader />
+      	{this.props.children}
+			</div>
 		);
 	}
 });
